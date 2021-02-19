33314ee1d8bd5db847b7fffcf135c89b8fbfa7c0
LucasBona05 <lucas.goncalves@ucsal.edu.br>
2021-02-19 16:34:08 -0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,79 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return (MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.blue[400],
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/lucas.png'),
+              ),
+              Text(
+                'Lucas Bonafini',
+                style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Flutter Developer'.toUpperCase(),
+                style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.cyan[100],
+                    fontFamily: 'SourceSansPro',
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.cyan[100]
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(Icons.phone, color: Colors.cyan[700]),
+                  title: Text(
+                    '+55 71 99157-2459',
+                    style: TextStyle(
+                        color: Colors.cyan[900],
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(Icons.mail, color: Colors.cyan[700]),
+                  title: Text(
+                    'lucasbona.goncalves@gmail.com',
+                    style: TextStyle(
+                        color: Colors.cyan[900],
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    ));
+  }
 }
